fcba59a5f9cef02eaa2da935a9a9cf9e4554d1f2
Miaritiana <hei.miaritiana@gmail.com>
2023-08-03 11:42:44 +0300

    feat: add transcript service

--- src/main/java/school/hei/haapi/repository/TranscriptRepository.java
+++ src/main/java/school/hei/haapi/repository/TranscriptRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import school.hei.haapi.model.Transcript;
 
+import java.util.List;
+
 @Repository
 public interface TranscriptRepository extends JpaRepository<Transcript, String> {
   @Query(value = "SELECT t FROM Transcript t WHERE t.id = :transcriptId " +
@@ -14,4 +16,6 @@ public interface TranscriptRepository extends JpaRepository<Transcript, String> 
           @Param("transcriptId") String transcriptId,
           @Param("studentId") String studentId
   );
+
+  List<Transcript> findTranscriptByStudentId(String studentId);
 }
