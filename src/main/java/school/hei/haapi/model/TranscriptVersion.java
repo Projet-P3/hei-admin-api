5a6c1739020148cf83cda1ec07e7664f1895915d
Miaritiana <hei.miaritiana@gmail.com>
2023-08-03 13:24:23 +0300

    chore: add validator to TranscriptVersion

--- src/main/java/school/hei/haapi/model/TranscriptVersion.java
+++ src/main/java/school/hei/haapi/model/TranscriptVersion.java
@@ -8,6 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -27,15 +28,21 @@ import org.hibernate.annotations.CreationTimestamp;
 public class TranscriptVersion {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @NotBlank(message = "id is mandatory")
   private String id;
   @ManyToOne()
   @JoinColumn(name = "transcript_id", referencedColumnName = "id")
+  @NotBlank(message = "transcriptId is mandatory")
   private Transcript transcriptId;
+  @NotBlank(message = "ref is mandatory")
   private int ref;
   @ManyToOne()
   @JoinColumn(name = "created_by_user_id", referencedColumnName = "id")
+  @NotBlank(message = "created_by_user_id is mandatory")
   private User createdByUserId;
+  @NotBlank(message = "created_by_user_role is mandatory")
   private String createdByUserRole;
   @CreationTimestamp
+  @NotBlank(message = "creation_datetime is mandatory")
   private Instant creationDatetime;
 }
