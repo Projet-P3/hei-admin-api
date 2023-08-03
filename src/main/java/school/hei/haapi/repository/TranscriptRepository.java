25c5c7901a4ea41ec45fce2f3f34524e4ac43d99
Miaritiana <hei.miaritiana@gmail.com>
2023-08-03 11:40:51 +0300

    feat: add findByTranscriptIdAndStudentId method

--- src/main/java/school/hei/haapi/repository/TranscriptRepository.java
+++ src/main/java/school/hei/haapi/repository/TranscriptRepository.java
@@ -1,9 +1,17 @@
 package school.hei.haapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import school.hei.haapi.model.Transcript;
 
 @Repository
 public interface TranscriptRepository extends JpaRepository<Transcript, String> {
+  @Query(value = "SELECT t FROM Transcript t WHERE t.id = :transcriptId " +
+    "AND t.studentId.id = :studentId")
+  Transcript findByTranscriptIdAndStudentId(
+    @Param("transcriptId") String transcriptId,
+    @Param("studentId") String studentId
+  );
 }
