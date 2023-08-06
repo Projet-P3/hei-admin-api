0998476db92fb757b0b5237aad459d776fafbbd0
MiRandria <hei.minosoa@gmail.com>
2023-08-06 22:40:08 +0300

    feat: add TranscriptClaim

--- src/main/java/school/hei/haapi/model/TranscriptVersion.java
+++ src/main/java/school/hei/haapi/model/TranscriptVersion.java
@@ -1,37 +1,31 @@
 package school.hei.haapi.model;
 
-import java.io.Serializable;
-import java.time.Instant;
-import java.util.Objects;
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import org.hibernate.Hibernate;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import school.hei.haapi.repository.types.PostgresEnumType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.time.Instant;
 
 import static javax.persistence.GenerationType.IDENTITY;
 
-@SuppressWarnings("checkstyle:WhitespaceAround")
+
 @Entity
 @Table(name = "\"transcript_version\"")
 @Getter
 @Setter
+@TypeDef(name = "pgsql_enum", typeClass = PostgresEnumType.class)
 @ToString
 @Builder
 @AllArgsConstructor
@@ -54,7 +48,5 @@ public class TranscriptVersion implements Serializable {
 
   @CreationTimestamp
   private Instant creationDatetime;
-
-
 
 }
