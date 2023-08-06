f2a0d86e152af596cfaf1fedf51bc2b4bc8991a8
miharyjoel <miharyjoel@gmail.com>
2023-08-06 18:21:37 +0300

    test: write claim_ok

--- src/test/java/school/hei/haapi/integration/StudentTranscriptVersionClaimIT.java
+++ src/test/java/school/hei/haapi/integration/StudentTranscriptVersionClaimIT.java
@@ -1,5 +1,6 @@
 package school.hei.haapi.integration;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
