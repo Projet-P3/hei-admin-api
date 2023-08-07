8501b9012288f12a24d83391b7d6edae39f99446
miharyjoel <miharyjoel@gmail.com>
2023-08-07 23:29:34 +0300

    fix: migration relation

--- src/main/resources/db/migration/V0_18__Create_student_transcript_version_claim_table.sql
+++ src/main/resources/db/migration/V0_18__Create_student_transcript_version_claim_table.sql
@@ -1,8 +1,8 @@
 do
 $$
 begin
-        if not exists(select from pg_type where typname = 'status') then
-create type "status" as enum ('OPEN','CLOSED');
+        if not exists(select from pg_type where typname = 'claim_status') then
+create type "claim_status" as enum ('OPEN','CLOSED');
 end if;
 end
 $$;
@@ -11,11 +11,10 @@ create table if not exists "transcript_version_claim"
 (
     id   varchar
         constraint transcript_version_claim_pk primary key default uuid_generate_v4(),
-    transcript_id       varchar
-        constraint transcript_id_fk references "transcript" (id),
+    transcript_id       varchar,
     version_id  varchar
         constraint transcript_version_id_fk references "transcript_version" (id),
-    status    status    not null,
+    status    claim_status    not null,
     creation_datetime    timestamp with time zone not null,
     closed_datetime    timestamp with time zone not null,
     reason varchar not null
