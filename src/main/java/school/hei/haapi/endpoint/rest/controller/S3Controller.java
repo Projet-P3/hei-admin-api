185197e8e8ceaf533e2d800e503683399a2345bb
Haritiana Adriano <hei.adriano.4@gmail.com>
2023-08-09 14:44:58 +0300

    fix: switch StudentTranscriptVersion to TranscriptVersion

--- src/main/java/school/hei/haapi/endpoint/rest/controller/S3Controller.java
+++ src/main/java/school/hei/haapi/endpoint/rest/controller/S3Controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import school.hei.haapi.endpoint.rest.mapper.TranscriptVersionMapper;
-import school.hei.haapi.endpoint.rest.model.StudentTranscriptVersion;
+import school.hei.haapi.endpoint.rest.model.TranscriptVersion;
 import school.hei.haapi.service.S3Service;
 
 import java.io.IOException;
@@ -19,7 +19,7 @@ public class S3Controller {
     private TranscriptVersionMapper mapper;
 
     @PostMapping("/students/{student_id}/transcripts/{transcript_id}/versions/latest/raw")
-    public StudentTranscriptVersion putStudentTranscriptVersionPdf(
+    public TranscriptVersion putStudentTranscriptVersionPdf(
             @PathVariable(name = "student_id") String studentId,
             @PathVariable(name = "transcript_id") String transcriptId,
             @RequestParam(name = "pdf")MultipartFile transcript_pdf
--- src/main/java/school/hei/haapi/endpoint/rest/mapper/TranscriptVersionMapper.java
+++ src/main/java/school/hei/haapi/endpoint/rest/mapper/TranscriptVersionMapper.java
@@ -1,13 +1,12 @@
 package school.hei.haapi.endpoint.rest.mapper;
 
 import org.springframework.stereotype.Component;
-import school.hei.haapi.endpoint.rest.model.StudentTranscriptVersion;
 import school.hei.haapi.model.TranscriptVersion;
 
 @Component
 public class TranscriptVersionMapper {
-    public StudentTranscriptVersion toRest(TranscriptVersion version) {
-        return new StudentTranscriptVersion()
+    public school.hei.haapi.endpoint.rest.model.TranscriptVersion toRest(TranscriptVersion version) {
+        return new school.hei.haapi.endpoint.rest.model.TranscriptVersion()
                 .transcriptId(version.getTranscript().getId())
                 .id(version.getId())
                 .ref(version.getRef())
