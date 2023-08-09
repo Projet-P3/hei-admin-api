9cf54c9bb6627d592a030c50c09f4507e169b73a
Haritiana Adriano <hei.adriano.4@gmail.com>
2023-08-09 15:06:08 +0300

    fix: migration cannot be executed 'cause two 16

--- src/main/resources/db/migration/V0_15__Create_transcript_version_table.sql
+++
@@ -1,8 +0,0 @@
-create table "transcript-version" if not exists (
-    id      varchar     primary key    not null,
-    transcript_id       varchar     references "transcript" (id),
-    ref     integer     not null,
-    create_by_user_id       varchar     references "user" (id),
-    create_by_user_role     varchar     not null,
-    creation_datetime    timestamp with time zone not null
-);
