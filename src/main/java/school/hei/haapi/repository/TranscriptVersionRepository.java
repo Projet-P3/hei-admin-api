ff8a148d68621e8ef39ecd812d3d61a9c9808557
Miaritiana <hei.miaritiana@gmail.com>
2023-08-09 22:55:48 +0300

    feat: update TranscriptVersionRepository and TranscriptVersionService

--- src/main/java/school/hei/haapi/repository/TranscriptVersionRepository.java
+++ src/main/java/school/hei/haapi/repository/TranscriptVersionRepository.java
@@ -1,31 +1,15 @@
 package school.hei.haapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import school.hei.haapi.model.Transcript;
 import school.hei.haapi.model.TranscriptVersion;
+
+import java.util.Optional;
 
 
 @Repository
 public interface TranscriptVersionRepository extends JpaRepository<TranscriptVersion, String> {
-    @Query("SELECT tv FROM TranscriptVersion tv " +
-            "JOIN tv.transcript t " +
-            "JOIN t.student s " +
-            "WHERE t.id = :transcriptId " +
-            "AND s.id = :studentId " +
-            "AND tv.id = :versionId")
-    TranscriptVersion getStudentTranscriptByVersionId(
-            @Param("transcriptId") String transcriptId,
-            @Param("studentId") String studentId,
-            @Param("versionId") String versionId
-    );
 
-    TranscriptVersion findByIdAndTranscript(String id, Transcript transcript);
+    Optional<TranscriptVersion> findByIdAndTranscriptId(String id, String transcriptId);
 
-
-    
-    
 }
--- src/main/java/school/hei/haapi/service/TranscriptVersionService.java
+++ src/main/java/school/hei/haapi/service/TranscriptVersionService.java
@@ -11,49 +11,40 @@ import school.hei.haapi.repository.TranscriptRepository;
 import school.hei.haapi.repository.TranscriptVersionRepository;
 import school.hei.haapi.repository.UserRepository;
 import school.hei.haapi.service.aws.S3Service;
-import school.hei.haapi.service.utils.FilePdfUtils;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Service;
-import school.hei.haapi.model.TranscriptVersion;
-import school.hei.haapi.repository.TranscriptVersionRepository;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class TranscriptVersionService {
 
-  private TranscriptVersionRepository repository;
-  private UserRepository userRepository;
-  private TranscriptRepository transcriptRepository;
-  private S3Service service;
-    public byte[] getTranscriptRaw(String studentId, String transcriptId, String versionId)  {
-
-    User student = userRepository.findById(studentId).get();
-    Transcript transcript = transcriptRepository.findById(transcriptId).get();
-    TranscriptVersion version = repository.findById(versionId).get();
-
-    String keyName = student.getRef()+"_"+transcript.getSemester()+"_v"+version.getRef();
-
-    return service.downloadPdfFromS3(keyName);
-  }
     private final TranscriptVersionRepository transcriptVersionRepository;
+    private TranscriptVersionRepository repository;
+    private UserRepository userRepository;
+    private TranscriptRepository transcriptRepository;
+    private S3Service service;
+
+    public byte[] getTranscriptRaw(String studentId, String transcriptId, String versionId) {
+
+        User student = userRepository.findById(studentId).get();
+        Transcript transcript = transcriptRepository.findById(transcriptId).get();
+        TranscriptVersion version = repository.findById(versionId).get();
+
+        String keyName = student.getRef() + "_" + transcript.getSemester() + "_v" + version.getRef();
+
+        return service.downloadPdfFromS3(keyName);
+    }
 
     public TranscriptVersion getStudentTranscriptByVersion(
             String studentId,
             String transcriptId,
             String versionId
     ) {
-        User user = userRepository.findById(studentId).get();
-        Transcript transcript = transcriptRepository.findById(transcriptId).get();
-
-        if(transcript.getStudent() == user) {
-          return repository.findByIdAndTranscript(versionId,transcript);
+        Optional<TranscriptVersion> transcriptVersion = repository.findByIdAndTranscriptId(versionId,transcriptId);
+        if(transcriptVersion.isPresent()){
+            return transcriptVersion.get();
         } else {
-          return null;
+            throw new NotFoundException("version not found");
         }
     }
 }
