78c2c819de98d68485b13d88c94052d608ffc1a3
Miaritiana <hei.miaritiana@gmail.com>
2023-08-09 22:55:48 +0300

    feat: update TranscriptVersionRepository and TranscriptVersionService

--- src/main/java/school/hei/haapi/repository/TranscriptVersionRepository.java
+++ src/main/java/school/hei/haapi/repository/TranscriptVersionRepository.java
@@ -2,7 +2,6 @@ package school.hei.haapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import school.hei.haapi.model.Transcript;
 import school.hei.haapi.model.TranscriptVersion;
 
 import java.util.Optional;
@@ -12,7 +11,5 @@ import java.util.Optional;
 public interface TranscriptVersionRepository extends JpaRepository<TranscriptVersion, String> {
 
     Optional<TranscriptVersion> findByIdAndTranscriptId(String id, String transcriptId);
-
-    TranscriptVersion findByIdAndTranscript(String id, Transcript transcript);
 
 }
