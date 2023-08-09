76f3b960fcc421f3f55160ec7ff93d3caa1ffced
Haritiana Adriano <hei.adriano.4@gmail.com>
2023-08-09 13:33:49 +0300

    fix: transcriptVersion relationToUser

--- src/main/resources/db/migration/V0_16__Create_transcript_version_table.sql
+++ src/main/resources/db/migration/V0_16__Create_transcript_version_table.sql
@@ -5,7 +5,8 @@ create table if not exists "transcript_version"
     transcript_id           varchar not null
     constraint transcript_version_transcript_id_fk references "transcript"(id),
     ref                     integer,
-    created_by_user_id      varchar not null,
+    created_by_user_id      varchar not null
+    constraint transcript_version_user_id_fk references "user"(id),
     created_by_user_role    varchar not null,
     creation_datetime       timestamp with time zone not null default now()
 );
