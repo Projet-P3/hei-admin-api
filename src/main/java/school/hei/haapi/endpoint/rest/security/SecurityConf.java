0729ea7a2563b430f8c5dad471553e06099d77cd
Ny Hasina VAGNO <hei.nyhasina@gmail.com>
2023-08-10 10:01:13 +0300

    fixup

--- src/main/java/school/hei/haapi/endpoint/rest/security/SecurityConf.java
+++ src/main/java/school/hei/haapi/endpoint/rest/security/SecurityConf.java
@@ -1,6 +1,5 @@
 package school.hei.haapi.endpoint.rest.security;
 
-import javax.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
