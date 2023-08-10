d0b64e9b81250442f7a31466a31abfc17af7bc2f
Miaritiana <hei.miaritiana@gmail.com>
2023-08-10 09:21:07 +0300

    feat: migrate JPQL to JPA in transcript repository and transcript-version repository

--- src/main/java/school/hei/haapi/repository/TranscriptRepository.java
+++ src/main/java/school/hei/haapi/repository/TranscriptRepository.java
@@ -7,14 +7,11 @@ import org.springframework.stereotype.Repository;
 import school.hei.haapi.model.Transcript;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TranscriptRepository extends JpaRepository<Transcript, String> {
-  @Query("SELECT t FROM Transcript t WHERE t.id = :transcriptId AND t.student.id = :studentId")
-  Transcript getByTranscriptIdAndStudentId(
-          @Param("transcriptId") String transcriptId,
-          @Param("studentId") String studentId
-  );
+  Optional<Transcript> findByIdAndStudentId(String id, String studentId);
 
   List<Transcript> findTranscriptByStudentId(String studentId);
 }
